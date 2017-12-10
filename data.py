8777f452a2e83395a635e59f28911b45acfbc6c6
ThisIsSoSteve <steve.caldwell22@gmail.com>
2017-12-10 21:43:59 +0000

    refactored the way the training data is stored and simplified the process

--- data.py
+++ data.py
@@ -1,22 +1,63 @@
 import numpy as np
 
 class Data:
-    def __init__(self, array_length):
-        
-        self.data = np.empty((0, array_length)) #dtype=np.int16 
+    def __init__(self, feature_length, label_length):
+        self.feature_length = feature_length
+        self.label_length = label_length
+        self.feature = np.empty((0, feature_length)) #dtype=np.int16 
+        self.label = np.empty((0, label_length))
 
-    def record(self, action, player_1, player_2):
-        label = np.zeros((1,10))
-        label[0, action-1] = 1 #one hot label
-        #label[0-3] = one hot action performed
+    '''
+    int action
+    Player user
+    Player opponent
+    bool is_user: is the the user(true) or the ai(false) recording?
 
-        label[0,4] = player_1.attack / player_1.max_attack
-        label[0,5] = player_1.defence / player_1.max_defence
-        label[0,6] = player_1.health / player_1.max_health
+    The is_user is there because we need to store the data from the AI's
+    perspective.
+    '''
+    def record(self, action, user, opponent, is_user):
+        new_feature = np.zeros((1, self.feature_length))
+        new_label = np.zeros((1, self.label_length))
 
-        label[0,7] = player_2.attack / player_2.max_attack
-        label[0,8] = player_2.defence / player_2.max_defence
-        label[0,9] = player_2.health / player_2.max_health
+        if is_user:
+            new_label[0, action - 1] = 1
+            user.last_action = new_label
 
-        self.data = np.append(self.data, label, axis=0)
-        #print(label)
+            #new_feature[0, opponent.last_action - 1] = 1 #one hot label
+            new_feature[0, 0] = opponent.last_action[0, 0]
+            new_feature[0, 1] = opponent.last_action[0, 1]
+            new_feature[0, 2] = opponent.last_action[0, 2]
+            new_feature[0, 3] = opponent.last_action[0, 3]
+
+
+            new_feature[0, 4] = opponent.attack / opponent.max_attack
+            new_feature[0, 5] = opponent.defence / opponent.max_defence
+            new_feature[0, 6] = opponent.health / opponent.max_health
+
+            new_feature[0, 7] = user.attack / user.max_attack
+            new_feature[0, 8] = user.defence / user.max_defence
+            new_feature[0, 9] = user.health / user.max_health
+
+            self.feature = np.append(self.feature, new_feature, axis=0)
+            self.label = np.append(self.label, new_label, axis=0)
+        else:
+            new_label[0, action - 1] = 1
+            opponent.last_action = new_label
+
+            #new_feature[0, user.last_action - 1] = 1 #one hot label
+            new_feature[0, 0] = opponent.last_action[0, 0]
+            new_feature[0, 1] = opponent.last_action[0, 1]
+            new_feature[0, 2] = opponent.last_action[0, 2]
+            new_feature[0, 3] = opponent.last_action[0, 3]
+
+            new_feature[0, 4] = user.attack / user.max_attack
+            new_feature[0, 5] = user.defence / user.max_defence
+            new_feature[0, 6] = user.health / user.max_health
+
+            new_feature[0, 7] = opponent.attack / opponent.max_attack
+            new_feature[0, 8] = opponent.defence / opponent.max_defence
+            new_feature[0, 9] = opponent.health / opponent.max_health
+
+            self.feature = np.append(self.feature, new_feature, axis=0)
+            self.label = np.append(self.label, new_label, axis=0)
--- main.py
+++ main.py
@@ -17,22 +17,31 @@ class Main:
         self.player_goes_first = True
         self.number_of_turns = 0
 
-        self.user = Player('user')
-        self.opponent = Player('opponent')
+        self.feature_length = 10
+        self.label_length = 4
+
+        self.user = Player('user', self.label_length)
+        self.opponent = Player('opponent', self.label_length)
         self.game_actions = Actions()
         #training data
-        self.player_training_data = Data(10)
-        self.opponent_training_data = Data(10)
+        self.player_training_data = Data(self.feature_length, self.label_length)
+        self.opponent_training_data = Data(self.feature_length, self.label_length)
 
         #checkpoint to use
         self.checkpoint = ''
 
-        self.training_data_x = np.empty((0, 10))
-        self.training_data_y = np.empty((0, 4))
-
-        self.starting_stats = []
+        self.starting_stats = np.array([0, 0, 0, 0,
+                                           self.user.attack / self.user.max_attack,
+                                           self.user.defence / self.user.max_defence,
+                                           self.user.health / self.user.max_health,
+                                           self.opponent.attack / self.opponent.max_attack,
+                                           self.opponent.defence / self.opponent.max_defence,
+                                           self.opponent.health / self.opponent.max_health])
 
         self.step = 0
+
+        self.training_data_x = np.empty((0, self.feature_length))
+        self.training_data_y = np.empty((0, self.label_length))
 
         self.logs_directory = "E:/Logs"
         shutil.rmtree(self.logs_directory)
@@ -45,12 +54,12 @@ class Main:
 
         self.number_of_turns = 0
 
-        self.user = Player('user')
-        self.opponent = Player('opponent')
+        self.user = Player('user', self.label_length)
+        self.opponent = Player('opponent', self.label_length)
 
         #reset training data
-        self.player_training_data = Data(10)
-        self.opponent_training_data = Data(10)
+        self.player_training_data = Data(self.feature_length, self.label_length)
+        self.opponent_training_data = Data(self.feature_length, self.label_length)
 
         os.system('cls')
 
@@ -61,34 +70,21 @@ class Main:
         except ValueError:
             return value, False
 
-    def train_ai(self, did_player_win, players_turn_first):
+    def train_ai(self, did_player_win):
         if did_player_win:
-            if players_turn_first:
-                self.opponent_training_data.data = np.insert(self.opponent_training_data.data, 0, self.starting_stats, axis=0)
-                
-            self.training_data_x = np.concatenate((self.training_data_x, self.opponent_training_data.data), axis=0)
-            self.training_data_y = np.concatenate((self.training_data_y, self.player_training_data.data[:, :4]), axis=0)
+            self.training_data_x = np.concatenate((self.training_data_x, self.player_training_data.feature), axis=0)
+            self.training_data_y = np.concatenate((self.training_data_y, self.player_training_data.label), axis=0)
         else:
-            if players_turn_first is False:
-                self.player_training_data.data = np.insert(self.player_training_data.data, 0, self.starting_stats, axis=0)
+            self.training_data_x = np.concatenate((self.training_data_x, self.opponent_training_data.feature), axis=0)
+            self.training_data_y = np.concatenate((self.training_data_y, self.opponent_training_data.label), axis=0)
 
-            self.training_data_x = np.concatenate((self.training_data_x, self.player_training_data.data), axis=0)
-            self.training_data_y = np.concatenate((self.training_data_y, self.opponent_training_data.data[:, :4]), axis=0)
+        print(self.training_data_x)
+        print(self.training_data_y)
 
         self.checkpoint, self.step = train.train_simple_model(self.training_data_x, self.training_data_y, self.checkpoint, self.step)
 
     def run_game(self):
         while(self.play_game):
-            if self.number_of_turns == 0:
-                #ToDo Move this out
-                self.starting_stats = np.array([0, 0, 0, 0,
-                                           self.user.attack / self.user.max_attack,
-                                           self.user.defence / self.user.max_defence,
-                                           self.user.health / self.user.max_health,
-                                           self.opponent.attack / self.opponent.max_attack,
-                                           self.opponent.defence / self.opponent.max_defence,
-                                           self.opponent.health / self.opponent.max_health])
-
             if self.player_turn:
                 self.user.print_health()
                 self.game_actions.display_player_actions(self.user)
@@ -102,8 +98,8 @@ class Main:
                     if players_action == 5:
                         self.play_game = False
                     else:
+                        self.player_training_data.record(players_action, self.user, self.opponent, True)
                         self.game_actions.perfrom(self.user, self.opponent, players_action)
-                        self.player_training_data.record(players_action, self.opponent, self.user) #Stores as AI would see ((ai, user) NOT (user, ai))
                         self.game_actions.display_player_chosen_action(self.user, players_action)
                         
                     self.player_turn = False
@@ -113,20 +109,33 @@ class Main:
             else: #AI's turn
 
                 data = self.starting_stats
-                if len(self.player_training_data.data) != 0:#if not self.player_training_data.data
-                    data = self.player_training_data.data[-1] 
+                if len(self.player_training_data.feature) != 0:#if not self.player_training_data.data
+                    #ToDo: find better way to do this
+                    data = np.array([self.user.last_action[0, 0],
+                                     self.user.last_action[0, 1],
+                                     self.user.last_action[0, 2],
+                                     self.user.last_action[0, 3],
+                                     self.user.attack / self.user.max_attack,
+                                     self.user.defence / self.user.max_defence,
+                                     self.user.health / self.user.max_health,
+                                     self.opponent.attack / self.opponent.max_attack,
+                                     self.opponent.defence / self.opponent.max_defence,
+                                     self.opponent.health / self.opponent.max_health
+
+                     ])
 
                 data = np.reshape(data, (-1, 10))
-                
-                ais_action = use.use_simple_model(self.checkpoint, data) + 1
-                print('opponent\'s choice number: {}'.format(ais_action))
+                print('opponents\'s view: {}'.format(data))
 
-                self.opponent_training_data.record(ais_action, self.user, self.opponent)
+                opponents_action = use.use_simple_model(self.checkpoint, data) + 1
+                print('opponent\'s choice number: {}'.format(opponents_action))
+
+                self.opponent_training_data.record(opponents_action, self.user, self.opponent, False)
 
                 print('')
                 self.opponent.print_health()
-                self.game_actions.display_ai_chosen_action(self.opponent, ais_action)
-                self.game_actions.perfrom(self.opponent, self.user, ais_action)
+                self.game_actions.display_ai_chosen_action(self.opponent, opponents_action)
+                self.game_actions.perfrom(self.opponent, self.user, opponents_action)
 
                 self.player_turn = True
 
@@ -137,11 +146,11 @@ class Main:
 
                 if self.user.alive is False:
                     os.system('cls')
-                    self.train_ai(False, self.player_goes_first)
+                    self.train_ai(False)
                     print('You lost')
                 else:
                     os.system('cls')
-                    self.train_ai(True, self.player_goes_first)
+                    self.train_ai(True)
                     print('You Won')
 
                 self.player_goes_first = not self.player_goes_first
@@ -155,3 +164,21 @@ class Main:
 #http://localhost:6006/
 
 Main().run_game()
+
+'''
+Training cycle
+User's go:
+    get opponent's previous move
+    get current state of opponent's and user's stats
+    get user's choice
+    store above data
+Opponents's go:
+    get user's previous move
+    get current state of user's and opponent's stats
+    get opponent's choice
+    store above data
+
+Train neural networks
+    get moves from the winner
+
+'''
